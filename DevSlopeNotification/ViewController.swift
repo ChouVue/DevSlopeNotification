a22621dbb34b7aff906d65aee6b934f6f5e7939d
ChouVue <30632978+ChouVue@users.noreply.github.com>
2018-01-24 17:56:07 -0600

    Video 125: 16min CheckPoint

--- DevSlopeNotification/ViewController.swift
+++ DevSlopeNotification/ViewController.swift
@@ -6,20 +6,90 @@
 //  Copyright © 2018 VUE. All rights reserved.
 //
 
+/* For Notification you must get permission from the user first*/
+
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //1.)First thing you need to do is REQUEST PERMISSION
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {
+            (granted, error) in
+            
+            //If they give us permission
+            if granted {
+                //Print Message
+                print("Notification Access Granted")
+            }
+            //And if it wasnt granted
+            else {
+                //Print the error message
+                print(error?.localizedDescription)
+            }
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyBtnPressed(sender: UIButton) {
+        
+        scheduleNotification(inSeconds: 5, completion: { success in
+            
+            if success {
+                print("Successfully Scheduled Notification")
+            }
+            else {
+                print("Error Scheduling Notification")
+            }
+        })
     }
-
-
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        let notif = UNMutableNotificationContent()
+        notif.title = "New Notification"
+        notif.subtitle = "These are working!"
+        notif.body = "First time developing Notification!"
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            
+            if error != nil{
+                print(error)
+                completion(false)
+            }
+            else {
+                completion(true)
+            }
+        })
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
